126bf37be19f8f1c75691efbc6ea5b1cc8b63d06
Chris Phillips <chrisof4@outlook.com>
2019-05-19 14:58:02 -0500

    Entry validation for quarters of 32nds is now working properly and the script exits if the entry is not valid. Rather than try to replace the value in the array, a value is now stored to $quarters based on the value of $entry_arr[2].

--- 5 Year Treasury Calculator.ps1
+++ 5 Year Treasury Calculator.ps1
@@ -19,20 +19,25 @@ function entry_price
 #Begin script execution
 entry_price
 [decimal[]]$entry_arr = $entry -split {$delim -contains $_}
-if ( $entry_arr[2] = 0) { 
-        $entry_arr[2] = 0 
+Write-Host "The value of $entry_arr[2] after the split is", $entry_arr[2]
+if ( $entry_arr[2] -eq 0) { 
+        $quarters = 0
+		write-host "if 0",$entry_arr[2]
         }
-      elseif ( $entry_arr[2] = 2 ) { 
-        $entry_arr[2] = .0078125 
+      elseif ( $entry_arr[2] -eq 2 ) { 
+        $quarters = .0078125 
+		write-host "if 2",$entry_arr[2]
         }
-      elseif ( $entry_arr[2] = 5 ) { 
-        $entry_arr[2] = (.0078125 * 2) 
-        }
-      elseif ( $entry_arr[2] = 7 ) { 
-        $entry_arr[2] = (.0078125 * 3) 
-        }
+      elseif ( $entry_arr[2] -eq 5 ) { 
+        $quarters = (.0078125 * 2) 
+        write-host "if 5",$entry_arr[2]
+		}
+      elseif ( $entry_arr[2] -eq 7 ) { 
+        $quarters = (.0078125 * 3) 
+        write-host "if 7",$entry_arr[2]
+		}
       else { throw "Enter a valid price" }
 
-$entry_dec = $entry_arr[0] + ($entry_arr[1] * .03125) + $entry_arr[2]
+$entry_dec = $entry_arr[0] + ($entry_arr[1] * .03125) + $quarters
 
 $entry_dec
