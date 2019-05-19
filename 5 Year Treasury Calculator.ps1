f9c86f53ba34d77cfdbfb2f1f37df501e0e50682
Chris Phillips <chrisof4@outlook.com>
2019-05-19 15:50:16 -0500

    The conversion was validated and is accurate. The script prompts for both an entry and exit price, validates both, converts them to decimal and displays them for the user. The format is very basic, but this is the most basic goal I was trying to achieve.

--- 5 Year Treasury Calculator.ps1
+++ 5 Year Treasury Calculator.ps1
@@ -10,39 +10,63 @@ $entry_arr = @()
 $entry_arr
 
 # Define functions
-function entry_price
+function user_input
 
        {
 		   [CmdletBinding()]
-              $script:entry = Read-Host "What is the entry price? Enter this in the format 999'99.99"
-
+				$script:entry = Read-Host "What is the entry price? Enter this in the format 999'99.99"
+				$script:exit = Read-Host "What is the exit price? Enter this in the format 999'99.99"
               }
 
 #Begin script execution
-entry_price
+user_input
 [decimal[]]$entry_arr = $entry -split {$delim -contains $_}
-Write-Host "The value of $entry_arr[2] after the split is", $entry_arr[2]
+[decimal[]]$exit_arr = $exit -split {$delim -contains $_}
+# Write-Host "The value of $entry_arr[2] after the split is", $entry_arr[2]
 
 # Data validation section
-# Validation of quarters of 32nds
-if ( $entry_arr[2] -eq 0) { 
-        $quarters = 0
-		write-host "if 0",$entry_arr[2]
-        }
-      elseif ( $entry_arr[2] -eq 2 ) { 
-        $quarters = .0078125 
-		write-host "if 2",$entry_arr[2]
-        }
-      elseif ( $entry_arr[2] -eq 5 ) { 
-        $quarters = (.0078125 * 2) 
-        write-host "if 5",$entry_arr[2]
-		}
-      elseif ( $entry_arr[2] -eq 7 ) { 
-        $quarters = (.0078125 * 3) 
-        write-host "if 7",$entry_arr[2]
-		}
-      else { throw "Enter a valid price" }
+if ($entry_arr[1] -lt 0 -or $entry_arr[1] -gt 31) {
+			throw "Enter a valid price"
+			}
+		elseif( $entry_arr[2] -eq 0) { 
+			$quarters_entry = 0
+			# write-host "if 0",$entry_arr[2]
+			}
+		elseif ( $entry_arr[2] -eq 2 ) { 
+			$quarters_entry = .0078125 
+			# write-host "if 2",$entry_arr[2]
+			}
+		elseif ( $entry_arr[2] -eq 5 ) { 
+			$quarters_entry = (.0078125 * 2) 
+			# write-host "if 5",$entry_arr[2]
+			}
+		elseif ( $entry_arr[2] -eq 7 ) { 
+			$quarters_entry = (.0078125 * 3) 
+			# write-host "if 7",$entry_arr[2]
+			}
+		else { throw "Enter a valid price" }
+if ($exit_arr[1] -lt 0 -or $exit_arr[1] -gt 31) {
+			throw "Enter a valid price"
+			}
+		elseif( $exit_arr[2] -eq 0) { 
+			$quarters_exit = 0
+			# write-host "if 0",$exit_arr[2]
+			}
+		elseif ( $exit_arr[2] -eq 2 ) { 
+			$quarters_exit = .0078125 
+			# write-host "if 2",$exit_arr[2]
+			}
+		elseif ( $exit_arr[2] -eq 5 ) { 
+			$quarters_exit = (.0078125 * 2) 
+			# write-host "if 5",$exit_arr[2]
+			}
+		elseif ( $exit_arr[2] -eq 7 ) { 
+			$quarters_exit = (.0078125 * 3) 
+			# write-host "if 7",$exit_arr[2]
+			}
+		else { throw "Enter a valid price" }
 
-$entry_dec = $entry_arr[0] + ($entry_arr[1] * .03125) + $quarters
-
-$entry_dec
+$entry_dec = $entry_arr[0] + ($entry_arr[1] * .03125) + $quarters_entry
+$exit_dec = $exit_arr[0] + ($exit_arr[1] * .03125) + $quarters_exit
+write-host "The entry $entry is",$entry_dec
+write-host "The exit $exit is",$exit_dec
