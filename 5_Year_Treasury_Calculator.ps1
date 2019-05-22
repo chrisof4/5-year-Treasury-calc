1f945c8ff6c27374b995b00e13c66f11ada151dd
Chris Phillips <ckphillips@jackhenry.com>
2019-05-22 15:56:09 -0500

    Stable version with do-while loop modification.

--- 5_Year_Treasury_Calculator.ps1
+++ 5_Year_Treasury_Calculator.ps1
@@ -76,88 +76,42 @@ if ($zone -lt 0) {
 		else {
 			Write-Host "`nThis is a long trade`n"
 			}
-$target1 = $entry_dec + $zone
-$target2 = $entry_dec + ($zone * 2)
-$target3 = $entry_dec + ($zone * 3)
-$target4 = $entry_dec + ($zone * 4)
-$target5 = $entry_dec + ($zone * 5)
 
-$target1_arr = $target1 -split {$delim -contains $_}
-$target1_32_arr = ($target1_arr[1]/312500) -split {$delim -contains $_}
-if ($target1_32_arr -eq 25) {
-    $target1_quarter = 2
-    }
-    elseif ($target1x_32_arr -eq 50) {
-    $target1_quarter = 5
-    }
-    elseif ($target1x_32_arr -eq 75) {
-    $target1_quarter = 7
-    }
-    else {$target1_quarter = 0}
-$target1_fr =  $target1_arr[0] + "'" + $target1_32_arr[0] + "." + $target1_quarter
-
-$target2_arr = $target2 -split {$delim -contains $_}
-$target2_32_arr = ($target2_arr[1]/312500) -split {$delim -contains $_}
-if ($target2_32_arr -eq 25) {
-    $target2_quarter = 2
-    }
-    elseif ($target2x_32_arr -eq 50) {
-    $target2_quarter = 5
-    }
-    elseif ($target2x_32_arr -eq 75) {
-    $target2_quarter = 7
-    }
-    else {$target2_quarter = 0}
-$target2_fr =  $target2_arr[0] + "'" + $target2_32_arr[0] + "." + $target2_quarter
-
-$target3_arr = $target3 -split {$delim -contains $_}
-$target3_32_arr = ($target3_arr[1]/312500) -split {$delim -contains $_}
-if ($target3_32_arr -eq 25) {
-    $target3_quarter = 2
-    }
-    elseif ($target3x_32_arr -eq 50) {
-    $target3_quarter = 5
-    }
-    elseif ($target3x_32_arr -eq 75) {
-    $target3_quarter = 7
-    }
-    else {$target3_quarter = 0}
-$target3_fr =  $target3_arr[0] + "'" + $target3_32_arr[0] + "." + $target3_quarter
-
-$target4_arr = $target4 -split {$delim -contains $_}
-$target4_32_arr = ($target4_arr[1]/312500) -split {$delim -contains $_}
-if ($target4_32_arr -eq 25) {
-    $target4_quarter = 2
-    }
-    elseif ($target4x_32_arr -eq 50) {
-    $target4_quarter = 5
-    }
-    elseif ($target4x_32_arr -eq 75) {
-    $target4_quarter = 7
-    }
-    else {$target4_quarter = 0}
-$target4_fr =  $target4_arr[0] + "'" + $target4_32_arr[0] + "." + $target4_quarter
-
-$target5_arr = $target5 -split {$delim -contains $_}
-$target5_32_arr = ($target5_arr[1]/312500) -split {$delim -contains $_}
-if ($target5_32_arr -eq 25) {
-    $target5_quarter = 2
-    }
-    elseif ($target5x_32_arr -eq 50) {
-    $target5_quarter = 5
-    }
-    elseif ($target5x_32_arr -eq 75) {
-    $target5_quarter = 7
-    }
-    else {$target5_quarter = 0}
-$target5_fr =  $target5_arr[0] + "'" + $target5_32_arr[0] + "." + $target5_quarter
+$x = 1
+DO
+{
+	New-Variable -name "target$x" -value ($entry_dec + ($zone * $x))
+	
+	$target = ($entry_dec + ($zone * $x))
+	$target_arr = ($target -split {$delim -contains $_})
+	$target_32_arr = ($target_arr[1]/312500) -split {$delim -contains $_}
+	if ($target_32_arr -eq 25) {
+		$target_quarter = 2
+		}
+		elseif ($target_32_arr -eq 50) {
+		$target_quarter = 5
+		}
+		elseif ($target_32_arr -eq 75) {
+		$target_quarter = 7
+		}
+		else {$target_quarter = 0}
+	New-Variable -name "target_fraction$x" -value ($target_arr[0] + "'" + $target_32_arr[0] + "." + $target_quarter)
+	$x++
+	} While ($x -le 5)
+$x--
 
 write-host "The entry $entry is",$entry_dec
 write-host "The exit $exit is",$exit_dec
-Write-Host "`nTarget 1x is", $target1_fr, $target1
-Write-Host "Target 2x is", $target2_fr, $target2
-Write-Host "Target 3x is", $target3_fr, $target3
-Write-Host "Target 4x is", $target4_fr, $target4
-Write-Host "Target 5x is", $target5_fr, $target5
+Write-Host "`nTarget 1x is", $target_fraction1, $target1
+Write-Host "Target 2x is", $target_fraction2, $target2
+Write-Host "Target 3x is", $target_fraction3, $target3
+Write-Host "Target 4x is", $target_fraction4, $target4
+Write-Host "Target 5x is", $target_fraction5, $target5
 
 $pause = Read-Host "`nPress any key to continue"
+DO
+	{
+		Remove-Variable "target$x"
+		Remove-Variable "target_fraction$x"
+		$x--
+	} While ($x -ge 1)
