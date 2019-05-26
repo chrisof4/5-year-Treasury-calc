d1812a9872b73160776f6b98c7617ff3447827a9
Chris Phillips <chrisof4@outlook.com>
2019-05-26 13:48:28 -0500

    Modified 5year.ps1. The program now calculates the confirmation entry and includes it in the display.

--- 5year.ps1
+++ 5year.ps1
@@ -13,7 +13,7 @@ $Entry_arr = @()
 # Create objects
 $EntryObj = New-Object -TypeName psobject
 $ExitObj = New-Object -TypeName psobject
-$ActivationObj = New-Object -TypeName psobject
+$ConfirmationObj = New-Object -TypeName psobject
 $TargetObj1 = New-Object -TypeName psobject
 $TargetObj2 = New-Object -TypeName psobject
 $TargetObj3 = New-Object -TypeName psobject
@@ -77,8 +77,44 @@ if ($Exit_arr[1] -lt 0 -or $Exit_arr[1] -gt 31) {
 
 $Entry_dec = $Entry_arr[0] + ($Entry_arr[1] * .03125) + $quarters_entry
 $Exit_dec = $Exit_arr[0] + ($Exit_arr[1] * .03125) + $quarters_exit
-$zone = $Entry_dec - $Exit_dec
-if ($zone -lt 0) {
+$Zone = $Entry_dec - $Exit_dec
+
+# -- Calculate the confirmation entry price, also known as the activation price. --
+# The confirmation price is roughly halfway between the entry and the exit.
+# However, this has to end in a quarter of a 32nd. This section generates an 
+# estimate, modifies the number so it properly ends in a quarter of a 32nd,
+# calculates the price as a fraction and then recalculates the fraction 
+# price into a decimal price.
+$Confirmation_estimate = $Entry_dec - ($Zone/2)
+[decimal[]]$Confirmation_est_arr = $Confirmation_estimate -split {$delim -contains $_}
+$Confirmation_32_arr = ($Confirmation_est_arr[1]/3125000) -split {$delim -contains $_}
+	if ($Confirmation_32_arr[1] -le 25) {
+		$Confirmation_quarter = 2
+		}
+		elseif ($Confirmation_32_arr[1] -le 50) {
+		$Confirmation_quarter = 5
+		}
+		elseif ($Confirmation_32_arr[1] -le 75) {
+		$Confirmation_quarter = 7
+		}
+		else {$Confirmation_quarter = 0}
+$Confirmation_fraction =  ([string]$Confirmation_est_arr[0] + "'" + [string]$Confirmation_32_arr[0] + "." + [string]$Confirmation_quarter)
+$Confirmation_arr = $Confirmation_fraction -split {$delim -contains $_}
+if ($Confirmation_arr[2] -eq 0) { 
+			$Quarters_confirmation = 0
+			}
+		elseif ( $Confirmation_arr[2] -eq 2 ) { 
+			$Quarters_confirmation = .0078125 
+			}
+		elseif ( $Confirmation_arr[2] -eq 5 ) { 
+			$Quarters_confirmation = (.0078125 * 2) 
+			}
+		elseif ( $Confirmation_arr[2] -eq 7 ) { 
+			$Quarters_confirmation = (.0078125 * 3) 
+			}
+$Confirmation_dec = $Confirmation_arr[0] + ($Confirmation_arr[1] * .03125) + $Quarters_Confirmation
+
+if ($Zone -lt 0) {
 			Write-Host "`nThis is a short trade"
 				}
 		else {
@@ -88,9 +124,9 @@ if ($zone -lt 0) {
 $x = 1
 DO
 {
-	New-Variable -name "target$x" -value ($Entry_dec + ($zone * $x))
+	New-Variable -name "target$x" -value ($Entry_dec + ($Zone * $x))
 	
-	$Target = ($Entry_dec + ($zone * $x))
+	$Target = ($Entry_dec + ($Zone * $x))
 	$Target_arr = ($Target -split {$delim -contains $_})
 	$Target_32_arr = ($Target_arr[1]/312500) -split {$delim -contains $_}
 	if ($Target_32_arr -eq 25) {
@@ -116,6 +152,10 @@ $ExitObj | Add-Member -MemberType NoteProperty -Name "Price Type" -Value Exit
 $ExitObj | Add-Member -MemberType NoteProperty -Name Fraction -Value $Exit
 $ExitObj | Add-Member -MemberType NoteProperty -Name Decimal -Value $Exit_dec
 
+$ConfirmationObj | Add-Member -MemberType NoteProperty -Name "Price Type" -Value Confirmation
+$ConfirmationObj | Add-Member -MemberType NoteProperty -Name Fraction -Value $Confirmation_fraction
+$ConfirmationObj | Add-Member -MemberType NoteProperty -Name Decimal -Value $Confirmation_dec
+
 $TargetObj1 | Add-Member -MemberType NoteProperty -Name "Price Type" -Value "Target 1x"
 $TargetObj1 | Add-Member -MemberType NoteProperty -Name Fraction -Value $Target_fraction1
 $TargetObj1 | Add-Member -MemberType NoteProperty -Name Decimal -Value $Target1
@@ -136,7 +176,7 @@ $TargetObj5 | Add-Member -MemberType NoteProperty -Name "Price Type" -Value "Tar
 $TargetObj5 | Add-Member -MemberType NoteProperty -Name Fraction -Value $Target_fraction5
 $TargetObj5 | Add-Member -MemberType NoteProperty -Name Decimal -Value $Target5
 
-Write-Output $EntryObj, $ExitObj, $TargetObj1, $TargetObj2, $TargetObj3, $TargetObj4, $TargetObj5 
+Write-Output $EntryObj, $ExitObj, $ConfirmationObj,$TargetObj1, $TargetObj2, $TargetObj3, $TargetObj4, $TargetObj5 
 
 DO
 	{
