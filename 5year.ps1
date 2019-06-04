e65c204e0b17ff0a0d9a6539c5cf1b5efd690de6
Chris Phillips <ckphillips@jackhenry.com>
2019-06-04 16:08:31 -0500

    Modified 5year.ps1. Added the variable $Distal and the code to collect this from the user. Simplified the text that requests input from the user.

--- 5year.ps1
+++ 5year.ps1
@@ -6,8 +6,9 @@
 
 Clear-Host
 Write-Output "Welcome To The Price Calculator For 5 Year Treasury Notes`n"
-$Entry = Read-Host "What is the entry price? Enter this in the format 999'99.99"
-$Exit = Read-Host "What is the exit price? Enter this in the format 999'99.99"
+$Entry = Read-Host "What is the entry price? (###'##.##)"
+$Distal = Read-Host "What is the distal price? (###'##.##)"
+$Exit = Read-Host "What is the exit price? (###'##.##)"
 [int]$Contracts = Read-Host "How many contracts?"
 
 # Define delimeters
