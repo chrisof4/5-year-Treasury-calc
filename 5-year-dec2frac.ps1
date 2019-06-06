ac7b5f1d6de86655d4264468a966698b7763e369
Chris Phillips <ckphillips@jackhenry.com>
2019-06-06 14:54:40 -0500

    Modified 5-year-dec2frac.ps1. Added the function to perform the conversion and display the result.

--- 5-year-dec2frac.ps1
+++ 5-year-dec2frac.ps1
@@ -1,1 +1,32 @@
-
+# File: 5-year-dec2frac.ps1
+# Author: Chris Phillips
+# Creation Date: 2019-06-06
+# Description: Converts 5 year treasury price from decimal to fraction
+function Calculate-Fractional {
+	[CmdletBinding()]
+    param($Decimal)
+    $delim = "'","."
+	
+		$Decimal_arr = ($Decimal -split {$delim -contains $_})
+        $Dec32 = "." + $Decimal_arr[1]
+		$Decimal_32_arr = [math]::Round(([decimal]($Dec32)*32),2) -split {$delim -contains $_}
+        
+		if ($Decimal_32_arr[1] -eq 25) {
+			$Decimal_quarter = 2
+			}
+		elseif ($Decimal_32_arr[1] -eq 50) {
+		$Decimal_quarter = 5
+		    }
+		elseif ($Decimal_32_arr[1] -eq 75) {
+		$Decimal_quarter = 7
+		    }
+		else {$Decimal_quarter = 0}
+		$Fraction = ($Decimal_arr[0] + "'" + $Decimal_32_arr[0] + "." + $Decimal_quarter)
+		return $Fraction
+}
+Clear-Host
+Write-Host "Welcome To The Price Convertor For 5 Year Treasury Notes"
+Write-Host "This will convert a decimal price to a fractional price`n"
+$DecPrice = Get-Input "What is the decimal price? (###.#######)"
+$FracPrice = Calculate-Fractional $DecPrice
+Write-Host "`n$DecPrice converts to $FracPrice"
